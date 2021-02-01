cf07667e3bd158adf959599793349b6e76b10c53
art01234 <magicdworf123@gmail.com>
2021-02-01 13:23:06 +0100

    initial commit

---
+++ src/main/java/com/art0123/LearnFromHome/LearnFromHomeApplication.java
@@ -0,0 +1,13 @@
+package com.art0123.LearnFromHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnFromHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnFromHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/art0123/LearnFromHome/LearnFromHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.art0123.LearnFromHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnFromHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
